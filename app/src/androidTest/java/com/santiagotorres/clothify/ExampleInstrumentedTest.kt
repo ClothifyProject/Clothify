6ff5cc3e1f3d37ee1f32fee036b5091ed04b58f3
santi <santiago.torresv>
2023-05-23 23:38:14 -0500

    Cambios en el gradle que no permitian crear la APK

--- app/src/androidTest/java/com/santiagotorres/clothify/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/santiagotorres/clothify/ExampleInstrumentedTest.kt
@@ -1,7 +1,9 @@
 package com.santiagotorres.clothify
 
-import androidx.test.platform.app.InstrumentationRegistry
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 
 import org.junit.Test
 import org.junit.runner.RunWith
