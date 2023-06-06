ea2ac4842fa2c6bae34e83572f2fbe1ca6aedefd
santi <santiago.torresv>
2023-06-06 14:11:22 -0500

    Se agrega la funcionalidad de agregar la foto en el perfil.

--- app/src/main/java/com/santiagotorres/clothify/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/santiagotorres/clothify/ui/perfil/PerfilFragment.kt
@@ -9,11 +9,26 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.santiagotorres.clothify.R
 import com.santiagotorres.clothify.databinding.FragmentPerfilBinding
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
 
 
 class PerfilFragment : Fragment() {
 
+    private val PICK_IMAGE_REQUEST = 1
+    private lateinit var imageView: ImageView
+
+
     private lateinit var perfilBinding: FragmentPerfilBinding
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,10 +51,31 @@ class PerfilFragment : Fragment() {
             }
         }
 
+        val buttonOpenGallery: Button = view.findViewById(R.id.buttonOpenGallery)
+        buttonOpenGallery.setOnClickListener {
+            openGallery()
+        }
+
         perfilBinding.imagenPerfil.setImageResource(R.drawable.imagen_perfil)
 
 
         return view
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri: Uri = data.data ?: return
+            //imageView.setImageURI(selectedImageUri)
+            perfilBinding.imagenPerfil.setImageURI(selectedImageUri)
+
+        }
     }
 
 }
